b1e1d9d4c2500e46058092f0351fb1a9fd590ee7
Butter Chilly <88847062+Butterchilly@users.noreply.github.com>
2023-12-05 19:01:43 +0530

    Notify Event Change

--- client/meth.lua
+++ client/meth.lua
@@ -1,3 +1,4 @@
+local base = require 'client.base'
 local QBCore = exports[Config.Resource_Names.Core]:GetCoreObject()
 
 -- \\ Start Cooking // --
@@ -14,17 +15,17 @@ RegisterNetEvent('CC-Drugtables:Client:Start_Cooking',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Start_Cooking, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Preparing_Goods, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Start_Cooking], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Preparing_Goods], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
         else
-            QBCore.Functions.Notify('You Dont Have Paint Thinner', 'error', 5000)
+           base:Notify('You Dont Have Paint Thinner', 'error', 5000)
     end
 end)
 
@@ -40,14 +41,14 @@ RegisterNetEvent('CC-Drugtables:Client:Preparing_Goods',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Preparing_Goods, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Combinen_Goods, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Preparing_Goods], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Combinen_Goods], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -63,14 +64,14 @@ RegisterNetEvent('CC-Drugtables:Client:Combinen_Goods',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Combinen_Goods, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Mix_In_Water, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Combinen_Goods], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Mix_In_Water], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -86,14 +87,14 @@ RegisterNetEvent('CC-Drugtables:Client:Mix_In_Water',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Mix_In_Water, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Add_Solvent, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Mix_In_Water], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Add_Solvent], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -109,14 +110,14 @@ RegisterNetEvent('CC-Drugtables:Client:Add_Solvent',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Add_Solvent, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Crystalize_Product, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Add_Solvent], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Crystalize_Product], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -132,14 +133,14 @@ RegisterNetEvent('CC-Drugtables:Client:Crystalize_Product',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Crystalize_Product, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Pack_Product, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Crystalize_Product], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Pack_Product], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -155,14 +156,14 @@ RegisterNetEvent('CC-Drugtables:Client:Pack_Product',function ()
         anim = 'machinic_loop_mechandplayer',
         flags = 16,
     }, {}, {}, function()
-        QBCore.Functions.Notify('Step Completed', 'success', 5000)
+       base:Notify('Step Completed', 'success', 5000)
 
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Items.Pack_Product, 1)
         TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Items.Meth, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Pack_Product], "remove")
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Items.Meth], "add")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -196,7 +197,7 @@ RegisterNetEvent('CC-Drugtables:Client:Spawn_Meth_Table', function()
         TriggerServerEvent('CC-Drugtables:server:RemoveItem', Config.Meth.Item, 1)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Meth.Item], "remove")
         end, function()
-            QBCore.Functions.Notify('Failed To Put ', 'error', 5000)
+           base:Notify('Failed To Put ', 'error', 5000)
         end)
 end)
 
@@ -220,7 +221,7 @@ RegisterNetEvent('CC-Drugtables:Client:Remove_Meth_Table', function(obj)
                     TriggerServerEvent('CC-Drugtables:server:AddItem', Config.Meth.Item, 1)
                     TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[Config.Meth.Item], "AddItem")
                 else
-                    QBCore.Functions.Notify("You can't pick up this table", 'error', 5000)
+                   base:Notify("You can't pick up this table", 'error', 5000)
                 end
             end
         end)
