3863496d578e0ae36215db1c27e5f1e62a85bfdc
Butter Chilly <88847062+Butterchilly@users.noreply.github.com>
2024-02-16 21:06:20 +0530

    Progressbar Changed To Lib/ Hasite Function Update

--- client/meth.lua
+++ client/meth.lua
@@ -3,151 +3,198 @@ local QBCore = exports[Config.Resource_Names.Core]:GetCoreObject()
 
 -- \\ Start Cooking // --
 RegisterNetEvent('Butter-Drugtables:Client:Start_Cooking',function ()
-    local hasItem = QBCore.Functions.HasItem(Config.Items.Start_Cooking)
-    if hasItem then
-    QBCore.Functions.Progressbar('Cooking', 'Cooking...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
+    local HasItem = Base:Hasitem(Config.Items.Start_Cooking, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Cooking...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
         Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Start_Cooking, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Preparing_Goods, 1)
-        end, function()
-           Base:Notify('Failed To Put ', 'error', 5000)
-        end)
-        else
-           Base:Notify('You Dont Have Paint Thinner', 'error', 5000)
+    else
+        Base:Notify('Failed To Coock ', 'error', 5000)
     end
+    else
+        Base:Notify('You Dont Have Paint Thinner', 'error', 5000)
+end
 end)
 
 -- \\ Preparing Goods // --
 RegisterNetEvent('Butter-Drugtables:Client:Preparing_Goods',function ()
-    QBCore.Functions.Progressbar('Preparing_Goods', 'Preparing Goods...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
+    local HasItem = Base:Hasitem(Config.Items.Preparing_Goods, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Preparing Goods...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Preparing_Goods, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Combinen_Goods, 1)
-        end, function()
-           Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    else
+        Base:Notify('Failed To Preparing Goods ', 'error', 5000)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Combinen Goods // --
 RegisterNetEvent('Butter-Drugtables:Client:Combinen_Goods',function ()
-    QBCore.Functions.Progressbar('Combinen_Goods', 'Combinen Goods...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
-
+    local HasItem = Base:Hasitem(Config.Items.Combinen_Goods, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Combinen Goods...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Combinen_Goods, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Mix_In_Water, 1)
-        end, function()
-           Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    else
+        Base:Notify('Failed To Combinen Goods ', 'error', 5000)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Mix In Water // --
 RegisterNetEvent('Butter-Drugtables:Client:Mix_In_Water',function ()
-    QBCore.Functions.Progressbar('Mix_In_Water', 'Mixing In Water...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
-
+    local HasItem = Base:Hasitem(Config.Items.Mix_In_Water, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Mixing In Water...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Mix_In_Water, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Add_Solvent, 1)
-        end, function()
-           Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    else
+           Base:Notify('Failed To Mix Water ', 'error', 5000)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Add Solvent // --
 RegisterNetEvent('Butter-Drugtables:Client:Add_Solvent',function ()
-    QBCore.Functions.Progressbar('Add_Solvent', 'Adding Solvent...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
-
+    local HasItem = Base:Hasitem(Config.Items.Add_Solvent, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Adding Solvent...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Add_Solvent, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Crystalize_Product, 1)
-        end, function()
+    else
            Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Crystalize Product // --
 RegisterNetEvent('Butter-Drugtables:Client:Crystalize_Product',function ()
-    QBCore.Functions.Progressbar('Crystalize_Product', 'Crystalize Product...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
+    local HasItem = Base:Hasitem(Config.Items.Crystalize_Product, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Crystalize Product...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Crystalize_Product, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Pack_Product, 1)
-        end, function()
+    else
            Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Pack Product // --
 RegisterNetEvent('Butter-Drugtables:Client:Pack_Product',function ()
-    QBCore.Functions.Progressbar('Pack_Product', 'Pack Product...', 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true
-    }, {
-        animDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
-        anim = 'machinic_loop_mechandplayer',
-        flags = 16,
-    }, {}, {}, function()
-       Base:Notify('Step Completed', 'success', 5000)
-
+    local HasItem = Base:Hasitem(Config.Items.Pack_Product, 1)
+    if HasItem then
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Pack Product...',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@',
+            clip = 'machinic_loop_mechandplayer'
+        },
+    }) then
+        Base:Notify('Step Completed', 'success', 5000)
         TriggerServerEvent('Butter-Drugtables:server:RemoveItem', Config.Items.Pack_Product, 1)
         TriggerServerEvent('Butter-Drugtables:server:AddItem', Config.Items.Meth, 1)
-        end, function()
+    else
            Base:Notify('Failed To Put ', 'error', 5000)
-        end)
+    end
+else
+    Base:Notify('You Dont Have Item To Make It', 'error', 5000)
+end
 end)
 
 -- \\ Spawn Table // --
